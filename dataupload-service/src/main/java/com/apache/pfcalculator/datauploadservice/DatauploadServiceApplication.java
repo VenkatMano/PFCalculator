d4c2037a0db7df83aa6b9da1bc093a5608db71b6
Venkatesh <Venkatesh.Mangalraj@flatironssolutions.com>
2020-06-24 15:44:04 +0530

    Completed config server setup

--- dataupload-service/src/main/java/com/apache/pfcalculator/datauploadservice/DatauploadServiceApplication.java
+++ dataupload-service/src/main/java/com/apache/pfcalculator/datauploadservice/DatauploadServiceApplication.java
@@ -3,6 +3,7 @@ package com.apache.pfcalculator.datauploadservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.scheduling.annotation.EnableScheduling;
